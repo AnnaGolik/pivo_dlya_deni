066034b560f1bb20f32b82ec72e445134527f7af
AnnaGolik <golik.av@phystech.edu>
2021-12-06 17:32:56 +0300

    комменты по генератору

--- generator.py
+++ generator.py
@@ -6,14 +6,14 @@ from random import choice, randrange
 
 RES = WIDTH, HEIGHT = 800, 602
 TILE = 100 #расстояние между стенами в лабиринте (размер клетки)
-cols, rows = WIDTH // TILE, HEIGHT // TILE
+cols, rows = WIDTH // TILE, HEIGHT // TILE #cols - строки, rows - ряды
 
 
 class Cell:
     def __init__(self, x, y):
         self.x, self.y = x, y
-        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
-        self.visited = False
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True} #какие стороны останутся у клетки
+        self.visited = False #изначально все клетки не посещены
         self.thickness = 4
 
     def draw(self, sc):
@@ -28,7 +28,7 @@ class Cell:
         if self.walls['left']:
             pygame.draw.line(sc, pygame.Color('black'), (x, y + TILE), (x, y), self.thickness)
 
-    def get_rects(self):
+    def get_rects(self): #рисуем стенки стенки
         rects = []
         x, y = self.x * TILE, self.y * TILE
         if self.walls['top']:
@@ -41,13 +41,13 @@ class Cell:
             rects.append(pygame.Rect( (x, y), (self.thickness, TILE) ))
         return rects
 
-    def check_cell(self, x, y):
+    def check_cell(self, x, y): #проверяем, какие клетки соседние, а какие нет
         find_index = lambda x, y: x + y * cols
-        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
+        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1: #не выходит ли за края поля
             return False
         return self.grid_cells[find_index(x, y)]
 
-    def check_neighbors(self, grid_cells):
+    def check_neighbors(self, grid_cells): #создаем массив соседних клеток
         self.grid_cells = grid_cells
         neighbors = []
         top = self.check_cell(self.x, self.y - 1)
@@ -65,7 +65,7 @@ class Cell:
         return choice(neighbors) if neighbors else False
 
 
-def remove_walls(current, next):
+def remove_walls(current, next): #убираем стенки клетки, чтоб создать стены лабиринта
     dx = current.x - next.x
     if dx == 1:
         current.walls['left'] = False
@@ -81,10 +81,10 @@ def remove_walls(current, next):
         current.walls['bottom'] = False
         next.walls['top'] = False
 
-def generate_maze():
-    grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
-    current_cell = grid_cells[0]
-    array = []
+def generate_maze(): #создаем лабиринт
+    grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)] #массив всех клеток
+    current_cell = grid_cells[0] #начальная клетка
+    stack = [] #будем здесь хранить посещенные клетки
     break_count = 1
 
     while break_count != len(grid_cells):
@@ -93,9 +93,9 @@ def generate_maze():
         if next_cell:
             next_cell.visited = True
             break_count += 1
-            array.append(current_cell)
+            stack.append(current_cell)
             remove_walls(current_cell, next_cell)
             current_cell = next_cell
-        elif array:
-            current_cell = array.pop()
+        elif stack: #если идти больше некуда, возвращаем клетки из списка посещенных назад и идет опять по ним
+            current_cell = stack.pop()
     return grid_cells
